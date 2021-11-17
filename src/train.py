aa6b70efb941f7dc2c142240b0a96d1f3bc154c8
Alex <alexander.til@mahle.com>
2021-11-17 08:00:30 +0000

    Changed SVC to Logistic Regression

--- src/train.py
+++ src/train.py
@@ -28,7 +28,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
-from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 import pickle
@@ -48,7 +48,7 @@ y = iris.iloc[:, -1].values
 # Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-model = SVC()
+model = LogisticRegression()
 model.fit(X_train, y_train)
 
 # Save Model Using Pickle
